af0c0b309f097234dad35cf9a8b5f6f8b56675d7
priyapatil1 <patil.d.priya@gmail.com>
2021-04-15 19:03:33 +0100

    Completes 1-6

--- part_1/1-3.sh
+++ part_1/1-3.sh
@@ -5,6 +5,10 @@
 
 # Image devopsdockeruh/simple-web-service:ubuntu will start a container that outputs logs into a file. Go inside the container and use tail -f ./text.log to follow the logs. Every 10 seconds the clock will send you a “secret message”.
 
+docker run -d -it --name secret-messages devopsdockeruh/simple-web-service:ubuntu
+
+docker exec -it secret-messages sh -c 'tail -f ./text.log'
+
 # Submit the secret message and command(s) given as your answer.
 
 ### Secret message is: 'You can find the source code here: https://github.com/docker-hy'
